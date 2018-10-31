35097ca699dc5ca43303c918c2627c06c8094ecc
haanmiba <haanmiba@gmail.com>
2018-10-31 13:40:48 -0400

    Cleaned up code for Utilities.py and updated about section on personal website.

--- Utilities.py
+++ Utilities.py
@@ -6,21 +6,27 @@ import fileinput
 from datetime import datetime
 
 
-ERROR_STR = 'usage: python Utilities.py [--generate | --publish | --unpublish] <path>\n'
+USAGE_STR = 'usage: python Utilities.py [--generate | --publish | --unpublish] <path>\n'
+MISSING_PATH_STR = 'error: missing <path>\n'
+INCORRECT_PATH_STR = 'error: <path> must start with `{root_folder}/`\n'
+UNKNOWN_ARG_STR = 'error: unknown option `{arg}`'
 
 
 def create_url(title):
     return '-'.join((re.sub('[^A-Za-z0-9_ ]+', '', title.strip().lower())).split(' '))
 
-def generate_draft():
-    post_title = input('Title of post: ')
-    post_url = create_url(post_title)
-    
-    categories = re.sub(' +', ' ', input('Categories (each separated by a space): ').lower().strip())
-    
+def get_current_date_time():
     now = datetime.now()
     post_date = now.strftime('%Y-%m-%d')
     post_time = now.strftime('%H:%M:%S -4000')
+    return post_date, post_time    
+
+def generate_draft():
+    post_title = input('Title of post: ')
+    post_url = create_url(post_title)
+    categories = re.sub(' +', ' ', input('Categories (each separated by a space): ').lower().strip())
+
+    post_date, post_time = get_current_date_time()
     post_url = post_date + '-' + post_url
 
     yml_header = '''---
@@ -37,68 +43,67 @@ preview: "Enter preview here."
             f.write(line.strip() + '\n')
 
 
-def move_file(path, dest):
-    file_name = path.split('/')[-1]
-    new_path = '{}/{}'.format(dest, file_name)
-    shutil.move(path, new_path)
+def publish(file_path, src='_drafts', dest='_posts'):
+    file_name = file_path.split('/')[-1]
 
-
-def publish(title):
-    url = title.split('/')[-1]
-
-    now = datetime.now()
-    post_date = now.strftime('%Y-%m-%d')
-    post_time = now.strftime('%H:%M:%S -4000')
-    for line in fileinput.input(title, inplace=True):
+    post_date, post_time = get_current_date_time()
+    for line in fileinput.input(file_path, inplace=True):
         if line.startswith('date:'):
             print('date: {} {}'.format(post_date, post_time))
         else:
             print(line, end='')
 
-    new_url = post_date + '-' + '-'.join(url.split('-')[3:])
-    os.rename(title, '_posts/{}'.format(new_url))
+    new_file_name = post_date + '-' + file_name[11:]
+    os.rename(file_path, '_posts/{}'.format(new_file_name))
+
+
+def move_file(file_name, src, dest):
+    src_file_path = '{}/{}'.format(src, file_name)
+    dest_file_path = '{}/{}'.format(dest, file_name)
+    shutil.move(src_file_path, dest_file_path)
 
 
 def main():
     if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print(ERROR_STR)
+        print(USAGE_STR)
         sys.exit(1)
 
     action = sys.argv[1].lower()
-    file_path = sys.argv[2] if len(sys.argv) >= 3 else None
+    rel_file_path = sys.argv[2] if len(sys.argv) >= 3 else None
+    file_name = rel_file_path.split('/')[-1] if rel_file_path else None
 
     try:
         if action == '--generate':
             generate_draft()
         elif action == '--publish':
-            if len(sys.argv) < 3 or not file_path:
-                print('error: missing <path>')
-                print(ERROR_STR)
+            if len(sys.argv) < 3 or not rel_file_path:
+                print(MISSING_PATH_STR)
+                print(USAGE_STR)
                 sys.exit(1)
 
-            if not file_path.startswith('_drafts/'):
-                print('error: <path> must start with `_drafts/`\n')
+            if not rel_file_path.startswith('_drafts/'):
+                print(INCORRECT_PATH_STR.format(root_folder='_drafts'))
                 sys.exit(1)
 
-            publish(file_path)
+            publish(rel_file_path)
         elif action == '--unpublish':
-            if len(sys.argv) < 3 or not file_path:
-                print('error: missing <path>')
-                print(ERROR_STR)
+            if len(sys.argv) < 3 or not rel_file_path:
+                print(MISSING_PATH_STR)
+                print(USAGE_STR)
                 sys.exit(1)
 
-            if not file_path.startswith('_posts/'):
-                print('error: <path> must start with `_posts/`\n')
+            if not rel_file_path.startswith('_posts/'):
+                print(INCORRECT_PATH_STR.format(root_folder='_posts'))
                 sys.exit(1)
 
-            move_file(file_path, dest='_drafts')
+            move_file(file_name, src='_posts', dest='_drafts')
         else:
-            print('error: unknown option `{}`'.format(action))
-            print(ERROR_STR)
+            print(UNKNOWN_ARG_STR.format(arg=action))
+            print(USAGE_STR)
             sys.exit(1)
 
     except FileNotFoundError as e:
-        print('error: {}\n'.format(str(e).split(']')[-1].strip()))
+        print('error: {}\n'.format(''.join(str(e).split(']')[1:]).strip()))
         sys.exit(1)
 
 
